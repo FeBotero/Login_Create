1fc08a8d662936995f493507098a31de33596162
Felipe Botero <frpbotero@gmail.com>
2023-02-06 19:12:09 -0400

    Create route getUsers

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
+const cors = require("cors");
 
 const port = process.env.PORTA;
 
 const url = process.env.URL;
 const client = new MongoClient(url);
-
 const dbName = process.env.DBNAME;
 
 async function main() {
@@ -16,18 +16,25 @@ async function main() {
   const db = client.db(dbName);
   const collectionUser = db.collection("users");
 
-  return { collectionUser };
+  app.use(cors());
+  app.use(express.json());
+
+  app.get("/", function (req, res) {
+    res.send("Hello World");
+  });
+
+  app.get("/user", async function (req, res) {
+    const user = await collectionUser.find().toArray();
+
+    res.send(user);
+  });
+
+  app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+  });
 }
 
 main()
   .then((data) => console.log(data))
   .catch((error) => console.log(error))
   .finally(() => client.close());
-
-app.get("/", function (req, res) {
-  res.send("Hello World");
-});
-
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
